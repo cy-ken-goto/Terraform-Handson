1da2b07c63f083d8e655ffb992509684fceda088
cy-ken-goto <ken_goto@cybird.co.jp>
2016-07-27 14:40:09 +0900

    s3ポリシー追加

--- 2_advance/aws_s3.tf
+++ 2_advance/aws_s3.tf
@@ -28,6 +28,20 @@ resource "aws_s3_bucket" "elb_log" {
     #         date = "2016-01-12"
     #     }
     # }
+    policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "AddPerm",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:*",
+      "Resource": "arn:aws:s3:::${var.app_name}-elb-log/*"
+    }
+  ]
+}
+POLICY
 
     tags {
         Name = "${var.app_name}-elb-log"
