a1948b43e2cbe94172ad9cc34a4e421119e6a389
cy-ken-goto <ken_goto@cybird.co.jp>
2016-07-27 17:04:37 +0900

    修正

--- 0_TODAYS_IAM/aws_iam.tf
+++ 0_TODAYS_IAM/aws_iam.tf
@@ -6,22 +6,10 @@ resource "aws_iam_access_key" "tf_handson_user_ak" {
     user = "${aws_iam_user.tf_handson_user.name}"
 }
 
-resource "aws_iam_group" "tf_handson" {
-    name = "tf_handson"
-}
-
-resource "aws_iam_group_policy" "tf_handson" {
-    name = "tf_handson"
-    group = "${aws_iam_group.tf_handson.id}"
+resource "aws_iam_user_policy" "tf_handson_user_ro" {
+    name = "tf_handson_user_ro"
+    user = "${aws_iam_user.tf_handson_user.name}"
     policy = "${file("aws_iam_group_policies/tf_handson.json")}"
-}
-
-resource "aws_iam_group_membership" "tf_handson" {
-    name = "tf_handson-membership"
-    users = [
-        "${aws_iam_user.tf_handson_user.name}",
-    ]
-    group = "tf_handson"
 }
 
 output "iam user access_key" {
